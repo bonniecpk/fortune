1dc7a35071800007e0f1c5d9d46613419610ab9b
Po Ki Chui <pchui@flatworldknowledge.com>
2014-10-06 23:43:27 -0400

    fixed x-axis with correct dates
    added currency drop down to control panel
    
    change domain name to exchange.fortune.me
    
    combine the javascript together (temporarily)
    
    able to display different currency charts (Need refactoring)
    
    removed graph.js

--- api.rb
+++ api.rb
@@ -1,7 +1,11 @@
 module Fortune
   class Api < Sinatra::Base
     get '/daily_rates/:symbol' do
-      Fortune::DailyRate.where(currency: params[:symbol].upcase).to_json
+      Fortune::DailyRate.asc(:date).where(currency: params[:symbol].upcase).to_json
+    end
+
+    get '/currencies' do
+      Fortune::Currency.all.asc(:name).to_json
     end
   end
 end
--- assets/js/views/line-chart.js
+++ assets/js/views/line-chart.js
@@ -2,7 +2,7 @@
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-  var parse = d3.time.format("%b %Y").parse;
+  var parse = d3.time.format("%Y-%m-%d").parse;
 
   // Scales and axes. Note the inverted domain for the y-scale: bigger is up!
   var x = d3.time.scale().range([0, width]),
@@ -23,22 +23,18 @@
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.price); });
   
-  //d3.json("/api/v1/daily_rates/eur", type, function(error, data) {
-  //d3.json("http://bl.ocks.org/mbostock/raw/1166403/6340d3eabb63f34d3509cbfb376d7b953748b944/readme.csv", type, function(error, data) {
-  
   var line_graph = function(data) {
-    var values = data;
     // Compute the minimum and maximum date, and the maximum price.
-    x.domain([values[0].date, values[values.length - 1].date]);
-    y.domain([d3.min(values, function(d) { return d.price; }), d3.max(values, function(d) { return d.price; }) ]).nice();
+    x.domain([data[0].date, data[data.length - 1].date]);
+    y.domain([d3.min(data, function(d) { return d.price; }), d3.max(data, function(d) { return d.price; }) ]).nice();
   
     // Add an SVG element with the desired dimensions and margin.
     var svg = d3.select("#graph").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      //.on("click", click);
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .on("click", click);
   
     // Add the clip path.
     svg.append("clipPath")
@@ -51,7 +47,7 @@
     svg.append("path")
       .attr("class", "area")
       .attr("clip-path", "url(#clip)")
-      .attr("d", area(values));
+      .attr("d", area(data));
     
     // Add the x-axis.
     svg.append("g")
@@ -69,32 +65,24 @@
     svg.append("path")
       .attr("class", "line")
       .attr("clip-path", "url(#clip)")
-      .attr("d", line(values));
+      .attr("d", line(data));
     
     // Add a small label for the symbol name.
     svg.append("text")
       .attr("x", width - 6)
       .attr("y", height - 6)
       .style("text-anchor", "end")
-      .text("Daily Currency Chart");
+      .text($("select").val() + " Chart");
     
     // On click, update the x-axis.
-    //function click() {
-    //  var n = values.length - 1,
-    //      i = Math.floor(Math.random() * n / 2),
-    //      j = i + Math.floor(Math.random() * n / 2) + 1;
-    //  x.domain([values[i].date, values[j].date]);
-    //  var t = svg.transition().duration(750);
-    //  t.select(".x.axis").call(xAxis);
-    //  t.select(".area").attr("d", area(values));
-    //  t.select(".line").attr("d", line(values));
-    //}
-  }
-  //});
-  
-  // Parse dates and numbers. We assume values are sorted by date.
-  function type(d) {
-    d.date = parse(d.date);
-    d.price = +d.price;
-    return d;
+    function click() {
+      var n = data.length - 1,
+          i = Math.floor(Math.random() * n / 2),
+          j = i + Math.floor(Math.random() * n / 2) + 1;
+      x.domain([data[i].date, data[j].date]);
+      var t = svg.transition().duration(750);
+      t.select(".x.axis").call(xAxis);
+      t.select(".area").attr("d", area(data));
+      t.select(".line").attr("d", line(data));
+    }
   }
